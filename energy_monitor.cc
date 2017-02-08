b276f5dac616d3e09c0f18a7672b9b308e90e4f2
Cyrus Omar <cyology@gmail.com>
2017-02-08 17:31:06 -0500

    changed plugin type and added ros imports

--- energy_monitor.cc
+++ energy_monitor.cc
@@ -1,10 +1,16 @@
 #include <gazebo/gazebo.hh>
 
+#include <thread>
+#include "ros/ros.h"
+#include "ros/callback_queue.h"
+#include "ros/subscribe_options.h"
+#include "std_msgs/Float32.h"
+
 namespace gazebo
 {
-  class EnergyMonitorPlugin : public WorldPlugin
+  class EnergyMonitorPlugin : public ModelPlugin
   {
-    public: EnergyMonitorPlugin() : WorldPlugin()
+    public: EnergyMonitorPlugin() : ModelPlugin()
             {
 				gzdbg << "Constructed energy_monitor." << "\n";
             }
@@ -14,6 +20,6 @@ namespace gazebo
 				gzdbg << "Loaded energy_monitor." << "\n";
             }
   };
-  GZ_REGISTER_WORLD_PLUGIN(EnergyMonitorPlugin)
+  GZ_REGISTER_MODEL_PLUGIN(EnergyMonitorPlugin)
 }
 
