ffcdf714f7725383817712b7f67ad38d0dee4b10
Ivan Ruchkin <iruchkin@cs.cmu.edu>
2017-03-07 15:53:29 -0500

    Fix of an off-by-one error in c2v.
    Fixes issue #1.

--- energy_monitor.cc
+++ energy_monitor.cc
@@ -141,11 +141,15 @@ namespace gazebo {
 		// used to send voltage sensor values to the robot (a main use case)
 		int voltage_of_charge(double charge) {
 			double pct = charge / battery_capacity;
-			double idx_dbl = pct * NUM_V_DATA;
+			double idx_dbl = pct * (NUM_V_DATA - 1); // -1 is important here (mapping from 0 to num-1), otherwise the index goes out of range for pct = 0 
 			int idx_int = round(idx_dbl);
 			// The highest voltage is idx 0 in v_data, so need 
 			// reverse this when calculating
-			return v_data[NUM_V_DATA - idx_int];
+			int idx_int_r = NUM_V_DATA -1 - idx_int; 
+			if (idx_int_r < 0 || idx_int_r > NUM_V_DATA-1)
+				gzdbg << "Error: voltage index " << idx_int_r << " out of bounds [0, " << NUM_V_DATA - 1 << "]\n"; 
+
+			return gazebo::v_data[idx_int_r];
 		}
 
 		// used to interpret the messed-up MIT-LL input (a crutch)
